5dfbff8229e0f149e2be3cb30ca22cc6f469d16e
PerChrister <perchrister96@gmail.com>
2021-01-14 10:07:01 +0100

    fix: cleaned up extra code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
       <div className="App">
-
+        My React App
       </div>
     </React.Fragment>
   );
