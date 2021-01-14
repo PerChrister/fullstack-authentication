8a2c5e8da70b07c1916941af758071453e4411ee
PerChrister <perchrister96@gmail.com>
2021-01-14 11:38:21 +0100

    fix: cleaned up register component

--- frontend/src/components/forms/RegisterForm.js
+++ frontend/src/components/forms/RegisterForm.js
@@ -26,11 +26,11 @@ const RegisterForm = (props) => {
     return (
         <form>
             <div>
-            <label>Username</label>
-                <input type="text" placeholder="Enter your username" onChange={onUsernameChanged}/>
+                <label>Username</label>
+                <input type="text" placeholder="Enter your username" onChange={onUsernameChanged} />
             </div>
             <div>
-            <label>Password</label>
+                <label>Password</label>
                 <input type="password" placeholder="Enter your password" onChange={onPasswordChanged} />
             </div>
             <div>
