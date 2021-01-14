29b8d2a8a30d8e4314c3efbeb36fe8bd006f8117
PerChrister <perchrister96@gmail.com>
2021-01-14 10:22:51 +0100

    feat: added code to components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Dashboard from './components/HomeComponents/Dashboard'
+import Home from './components/HomeComponents/Home'
+import Login from './components/AuthComponents/Login'
+import LoginPhone from './components/AuthComponents/Register'
+import Register from './components/AuthComponents/LoginPhone'
 import './App.css';
 
 function App() {
@@ -8,6 +12,10 @@ function App() {
       <div className="App">
         My React App
         <Dashboard />
+        <Home />
+        <Login />
+        <LoginPhone />
+        <Register />
       </div>
     </React.Fragment>
   );
---
+++ frontend/src/components/AuthComponents/LoginPhone.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPhone = () => {
+
+    return (
+        <div>
+            <h1>Login Phone</h1>
+        </div>
+    )
+
+}
+
+export default LoginPhone;
